fce3b109f64460b866c34d622d6d0fdfab2f2d22
Johnathan Bergman <jbergman@bsd220.org>
2021-02-10 18:08:25 -0600

    Created list of Things

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List  {
+                Text("First thing")
+                Text("Second thing")
+                Text("Third thing")
+                Text("Fourth thing")
+                Text("Fifth thing")
+            }
+            .navigationBarTitle("Things")
+        }
     }
 }
 
