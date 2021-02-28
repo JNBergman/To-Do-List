82c7c5d9eb44d9c8a687b19fd95996543b106fae
Johnathan Bergman <jbergman@bsd220.org>
2021-02-28 13:38:13 -0600

    Redesigned List display

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,22 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var toDoItems =
+        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
         NavigationView {
             List  {
-                Text("First thing")
-                Text("Second thing")
-                Text("Third thing")
-                Text("Fourth thing")
-                Text("Fifth thing")
+                ForEach(toDoItems) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    toDoItems.remove(atOffsets: indexSet)
+                })
             }
-            .navigationBarTitle("Things")
+            .navigationBarTitle("To Do List")
+            .navigationBarItems(leading: EditButton())
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct ToDoItem: Identifiable {
+        var id = UUID()
+        var priority = String()
+        var description = String()
+        var dueDate = Date()
     }
 }
